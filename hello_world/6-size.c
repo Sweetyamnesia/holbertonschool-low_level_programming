e072cc074df5f50bfa22a778ce7395a76c62e0c7
Sweetyamnesia <angela.r@live.fr>
2025-02-17 14:46:29 +0000

    add file

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+/**
+* main - Entry point
+* Description : The main function will be called without any parameters.
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("the size of various types on the computer are %zu\n", sizeof(n));
+return (0);
+}
