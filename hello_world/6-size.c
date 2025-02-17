e267fede969435939a7420907e874adab33cfd42
Sweetyamnesia <angela.r@live.fr>
2025-02-17 15:08:18 +0000

    modif file suite warning betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
 * main - Entry point
@@ -8,6 +7,10 @@
 
 int main(void)
 {
-printf("the size of various types on the computer are %zu\n", sizeof(n));
+printf("Size of a char %zu\n", sizeof(char))
+printf("Size of an int %zu\n", sizeof(int))
+printf("Size of a long int %zu\n", sizeof(long int))
+printf("Size of a long long int %zu\n", sizeof(long long int))
+printf("Size of a float %zu\n", sizeof(float)
 return (0);
 }
