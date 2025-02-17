7048d7eeed4437cf4efe0f36bd504af0ddef43ae
Sweetyamnesia <angela.r@live.fr>
2025-02-17 14:08:06 +0000

    modif file : ajout description selon la doc betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+* main - Entry point
+* Description : The main function will be called without any parameters.
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
