d0149f9a3b3db82ec18c66621c1414fc32d33a5b
Sweetyamnesia <angela.r@live.fr>
2025-02-17 16:44:37 +0000

    installation du package et modif file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-printf("Size of a char %zu\n", sizeof(char))
-printf("Size of an int %zu\n", sizeof(int))
-printf("Size of a long int %zu\n", sizeof(long int))
-printf("Size of a long long int %zu\n", sizeof(long long int))
-printf("Size of a float %zu\n", sizeof(float)
+printf("Size of a char: %lu bytes\n", sizeof(char));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a long: int %lu bytes\n", sizeof(long int));
+printf("Size of a long: long int %lu bytes\n", sizeof(long long int));
+printf("Size of a float: %lu bytes\n", sizeof(float);
 return (0);
 }
