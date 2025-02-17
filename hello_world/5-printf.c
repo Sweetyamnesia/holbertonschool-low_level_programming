69ac6931388d5cb60794adef00749be0518f4a44
Sweetyamnesia <angela.r@live.fr>
2025-02-17 14:15:25 +0000

    add file

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description : The main function will be called without any parameters.
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
