3b84832fa205b07d832fd4a3b266c068adef43d4
Sweetyamnesia <angela.r@live.fr>
2025-02-19 10:15:46 +0000

    first commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Description : The main function will be called without any parameters.
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
