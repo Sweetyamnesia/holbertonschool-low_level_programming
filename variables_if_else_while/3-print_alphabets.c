38934a4e1502f00b433508e630f9e81c7e5462ef
Sweetyamnesia <angela.r@live.fr>
2025-02-19 09:45:00 +0000

    modif file

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,17 @@
 */
 
 int main(void)
+
 {
-int ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
 putchar(ch);
+}
 putchar ('\n');
 for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
 putchar(ch);
+}
 return (0);
 }
