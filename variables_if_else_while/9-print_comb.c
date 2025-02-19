475e98804f7deb8320aff2e332eb6fa2297eb370
Sweetyamnesia <angela.r@live.fr>
2025-02-19 14:17:31 +0000

    modif file 7 : changement de sens >=

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
 * main - Entry point
 * Description : The main function will be called without any parameters.
@@ -13,10 +12,7 @@ int main(void)
 int n;
 for (n = '0'; n <= '9'; n++)
 {
-putchar("%x");
-}
-{
-putchar(char);
+putchar(n);
 }
 putchar('\n');
 return (0);
