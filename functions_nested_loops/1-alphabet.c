8e0980a90801f8cd2c2ca18f4aff8e6861051f83
Sweetyamnesia <angela.r@live.fr>
2025-02-21 10:29:21 +0000

    modif file suite compilation

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* main - Entry point
-* Description : The main function will be called without any parameters.
+* print_alphabet - use to print alphabet
+* Description: The main function will be called without any parameters.
 *Return: Always 0 (Success)
-* _putchar - Prints a character to the standard output.
 */
 
-int main(void)
+void print_alphabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
@@ -16,5 +15,4 @@ for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 }
 _putchar('\n');
-return (0);
 }
