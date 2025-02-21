0e1745d88794cefc0274c24379958855f469cc7e
Sweetyamnesia <angela.r@live.fr>
2025-02-21 09:00:41 +0000

    modif file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,15 +5,27 @@
 * main - Entry point
 * Description : The main function will be called without any parameters.
 *Return: Always 0 (Success)
+* _putchar - Prints a character to the standard output.
 */
+
+int _putchar(char c)
+{
+int n = 0;
+n = write(1, &c, 1);
+return (n);
+}
 
 int main(void)
 {
-const char *str = "_putchar";
-while (*str)
-{
-putchar(*str++);
-}
-putchar('\n');
+int m = 0;
+m = _putchar('_');
+m = _putchar('p');
+m = _putchar('u');
+m = _putchar('t');
+m = _putchar('c');
+m = _putchar('h');
+m = _putchar('a');
+m = _putchar('r');
+m = _putchar('\n');
 return (0);
 }
