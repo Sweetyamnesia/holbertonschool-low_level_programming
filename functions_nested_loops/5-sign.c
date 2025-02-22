603110d675ca371251286f5589e6a83d3cb8b41c
Sweetyamnesia <angela.r@live.fr>
2025-02-22 14:51:46 +0000

    modif file 7

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
+_putchar(30);
 return (1);
 }
 else if (n == 0)
 {
-printf("0");
+_putchar(0);
 return (0);
 }
 else
 {
-printf("-");
+_putchar(50);
 return (-1);
 }
 }
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,9 @@
 int print_last_digit(int num)
 {
 int last_digit = num % 10;
-printf("%d\n", last_digit);
-return last_digit;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + '0');
+return (last_digit);
 }
 
