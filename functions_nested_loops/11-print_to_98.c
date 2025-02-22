233554070a4f66de7c3a8fb22f904866485d8e2e
Sweetyamnesia <angela.r@live.fr>
2025-02-22 16:02:33 +0000

    add files

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* print_to_98 - function that prints all numbers from n to 98.
+* @n: number.
+**/
+
+void print_to_98(int n)
+{
+int i = n;
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i < 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
