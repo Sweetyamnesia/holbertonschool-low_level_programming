84d6bb25abea6b92fabd84062b3ceecd1f50db3d
Sweetyamnesia <angela.r@live.fr>
2025-02-22 11:57:20 +0000

    modif file

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
-return 1;
+  printf("%s +\n", "");
+return (1);
 }
 else if (n == 0)
 {
-printf("0");
-return 0;
+  printf("%s 0\n", "");
+return (0);
 }
 else
 {
-printf("-");
-return -1;
+  printf("%s -\n", "");
+return (-1);
 }
 }
