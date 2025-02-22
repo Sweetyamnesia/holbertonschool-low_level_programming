b13d53fdefe8d0147ef067499522d59453b47310
Sweetyamnesia <angela.r@live.fr>
2025-02-22 13:41:05 +0000

    modif file 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,17 +3,12 @@
 #include "main.h"
 #include <ctype.h>
 /**
-* main - Entry point
-* Description : The main function will be called without any parameters.
-* int _abs - function that computes the absolute value of an integer.
-*Return: Always 0 (Success)
+* _abs - Computes the absolute value of an integer.
+* @n: The integer to be checked.
+* Return: The absolute value of the number.
 */
 
-int _abs(int)
+int _abs(int n)
 {
-int x, y;
-x = _abs(-10);
-printf("Absolute of -10: %d\n", x);
-y = abs(12);
-printf("Absolute of 12: %d", y);
+return (n >= 0 ? n : -n);
 }
