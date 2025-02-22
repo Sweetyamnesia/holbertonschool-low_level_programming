62360daa2a53540bca96e72df29615eb9f5ac39a
Sweetyamnesia <angela.r@live.fr>
2025-02-22 11:01:56 +0000

    add file

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* _isalpha - Checks if the character is a letter, lowercase or uppercase.
+* @c: The character to be checked.
+*
+* Return: 1 if c is lowercase, 0 otherwise.
+*/
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
