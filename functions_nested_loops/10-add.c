4512f1208663f62439d900f7fddf2bb3f577af95
Sweetyamnesia <angela.r@live.fr>
2025-02-22 17:25:27 +0000

    delete and modify code

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -8,27 +8,6 @@
 * Return: The sum of the two integers.
 **/
 
-int add(int a, int b);
-
-int main(void)
-{
-int num1, num2, sum;
-printf("Enter the first number: ");
-scanf("%d", &num1);
-printf("Enter the second number: ");
-scanf("%d", &num2);
-sum = add(num1, num2);
-printf("The sum of %d and %d is %d\n", num1, num2, sum);
-return (0);
-}
-
-/**
-* add - Computes the sum of two integers.
-* @a: The first integer to be added.
-* @b: The second integer to be added.
-* Return: The sum of the two integers.
-**/
-
 int add(int a, int b)
 {
 return (a + b);
