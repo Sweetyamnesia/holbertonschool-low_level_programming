3750118eb0c1ba30e8ad802381278d0dc2291cf9
Sweeryamnesia <angela.r@live.fr>
2025-02-24 21:24:31 +0000

    add files

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,14 @@
 
 void print_diagonal(int n)
 {
-int i = 0;
+int i;
+if (n <= 0)
+{
 for (i = 0; i < n; i++)
 {
-if (n == 0)
-{
-_putchar('\\');
+_putchar(' ');
 }
-_putchar('\\');
+_putchar(92);
 }
 _putchar('\n');
 }
