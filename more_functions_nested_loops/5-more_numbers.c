5004ad4abd4b9d1cf5189e861e16d488fc18a749
Sweetyamnesia <angela.r@live.fr>
2025-02-24 10:43:29 +0000

    first try

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* more_numbers - prints the numbers from 0 to 14.
+**/
+
+void more_numbers(void)
+{
+int n = 0;
+for (n = 0; n <= 14; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
