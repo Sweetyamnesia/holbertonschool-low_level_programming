4b924f8583d9ad589e9f45f5ec90b61809944920
Sweetyamnesia <angela.r@live.fr>
2025-02-24 10:23:42 +0000

    add file

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* print_most_numbers - prints the numbers from 0 to 9.
+**/
+
+void print_most_numbers(void)
+{
+int n = 0;
+for (n = 0; n <= 9; n++)
+{
+if (n != 2 && n != 4)
+{
+_putchar(48 + n);
+}
+}
+}
