df9efa6bed96b9a30aec0f3d3326d2ad9dd3cb04
Sweeryamnesia <angela.r@live.fr>
2025-02-24 19:39:53 +0000

    modif file

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,13 @@
 void print_line(int n)
 {
 int i = 0;
-for (n = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
-if (n <= 0)
+if (n == 0)
 {
 _putchar('_');
 }
-_putchar('\n');
+_putchar('_');
 }
+_putchar('\n');
 }
