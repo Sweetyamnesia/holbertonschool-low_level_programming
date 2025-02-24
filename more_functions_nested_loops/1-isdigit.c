20bb5a1ff447f8e7e9f612c27ae0acdcdb63a010
Sweetyamnesia <angela.r@live.fr>
2025-02-24 09:02:18 +0000

    add file

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* _isdigit - This function test if a character is a hexadecimal number.
+* @c: the character to test.
+* Return: return 1 if c is a digit and otherwise return 0.
+**/
+
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
