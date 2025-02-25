675b60699ca95ba32f37c33c050a67e151c4f0ce
Sweetyamnesia <angela.r@live.fr>
2025-02-25 14:04:15 +0000

    add files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+# include "main.h"
+
+/**
+* reset_to_98 - Function that takes a pointer to an int as parameter.
+* @n: the parameter of the function.
+**/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
