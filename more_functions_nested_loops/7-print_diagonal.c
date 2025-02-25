dc0ddbccaebde4b1c27587495aec0ec1c7036b97
Sweetyamnesia <angela.r@live.fr>
2025-02-25 08:40:27 +0000

    modif file

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ void print_diagonal(int n)
 int i;
 if (n <= 0)
 {
-for (i = 0; i < n; i++)
+for (i = 0; i > n; i++)
 {
-_putchar(' ');
+_putchar(32);
 }
 _putchar(92);
 }
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,27 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz\n");
+printf("FizzBuzz");
 }
-else if (i % 3 == 0)
+else if (i % 3 == 0) 
 {
-printf("Fizz\n");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz\n");
+printf("Buzz");
 }
 else
 {
-printf("%d\n", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 }
+printf("\n");
 return (0);
 }
