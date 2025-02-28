793effce8c6e7a2d604cfdf4b53c1c42a33959dc
Sweetyamnesia <angela.r@live.fr>
2025-02-28 09:32:36 +0000

    first commit

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* puts2 - function that print every other character of a string.
+* @str: the parameter.
+**/
+
+void puts2(char *str)
+{
+int number = 1;
+int i = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && number)
+printf("%c ", str[i]);
+number = 0;
+}
+if (str[i] == ' ')
+number = 1;
+}
