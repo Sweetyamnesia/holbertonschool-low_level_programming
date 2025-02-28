25f2231f5fbb3436388a12c523a7596638a60e7b
Sweetyamnesia <angela.r@live.fr>
2025-02-28 09:08:01 +0000

    modif file

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,19 @@
 
 void rev_string(char *s)
 {
-{
-int length;
-length = 0;
-while (*s != '\0')
+int length = 0;
+int i = 0;
+char *ptr = s;
+while (*ptr != '\0')
 {
 length++;
-s++;
+ptr++;
 }
-length--;
-for (; length >= 0; length--)
+s = ptr - length;
+for (i = 0;  i < length / 2; i++)
 {
-s--;
-}
+char temp = s[i];
+s[i] = s[length -i -1];
+s[length - i - 1] = temp;
 }
 }
