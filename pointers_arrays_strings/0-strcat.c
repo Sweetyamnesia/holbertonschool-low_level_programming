81eea3a029055a778aa99a113fbe3b7c0704146a
Sweetyamnesia <angela.r@live.fr>
2025-03-02 13:02:28 +0000

    add file, modif file and delete branch

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcat - function that concatenates two strings.
+* @dest: first string.
+* @src: second string.
+* Return: the address of dest.
+**/
+
+char *_strcat(char *dest, char *src)
+{
+char *ret = dest;
+while (*src)
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (ret);
+}
