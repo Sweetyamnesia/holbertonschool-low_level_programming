aa220c7ab3ddeacceb05a15a192d9ea661e13663
Sweetyamnesia <angela.r@live.fr>
2025-03-02 09:12:29 +0000

    modif file

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,8 +15,8 @@ while (str[length] != '\0')
 {
 length++;
 }
-start = (length % 2 == 0) ? (length / 2) : ((length + 1) / 2);
-for (i = start; i < length; i ++)
+start = (length + 1) / 2;
+for (i = start; i < length; i++)
 {
 _putchar(str[i]);
 }
