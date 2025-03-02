56dc3cb457f4107ddfd8bd917e547d89fe4707e3
Sweetyamnesia <angela.r@live.fr>
2025-03-02 10:11:59 +0000

    modif betty-doc

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,7 @@
 * _strcpy - function that copies the string pointed to by src
 * @dest: the buffer.
 * @src: the string pointed.
+* Return: the address of dest.
 **/
 
 char *_strcpy(char *dest, char *src)
