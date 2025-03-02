8428a7f3a8424fd11018d52b1056adaa260aee42
Sweetyamnesia <angela.r@live.fr>
2025-03-02 09:43:18 +0000

    modif file

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,13 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 1; i < n; i+= 1)
+for (i = 0; i < n; i += 1)
 {
-printf("%d",a[i]);
+printf("%d", a[i]);
 }
+if (i < n - 1)
+{
+printf(", ");
+}
+printf("\n");
 }
