b6568c10dfd8f0d77456bbd7022f2d16f5bd63c4
Sweetyamnesia <angela.r@live.fr>
2025-03-03 13:49:12 +0000

    modif file via terminal

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -7,9 +7,15 @@
 * @s: first parameter.
 * @b: second parameter.
 * @n : third parameter.
+* Return: show the result of s.
 **/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  
+unsigned int i = 0;
+while (i < n)
+{
+s[i++] = b;
+}
+return (s);
 }
