2701d15fb0baed13aefe2a82540656d0c0aced3d
Sweetyamnesia <angela.r@live.fr>
2025-03-03 14:57:25 +0000

    modif file

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,9 +3,10 @@
 #include <string.h>
 
 /**
-* _strchr - the function that locates a character in a string. 
-* @s: the string.
-* @c: the character. 
+* _strchr - the function that locates a character in a string.
+* @s: the pointer to the string.
+* @c: the character.
+* Return: NULL if the character is not found.
 **/
 
 char *_strchr(char *s, char c)
