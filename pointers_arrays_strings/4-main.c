d4edad3e1867e1bfa06c027b2d112932fcb3b01b
Sweetyamnesia <angela.r@live.fr>
2025-03-03 21:30:38 +0000

    add files

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -14,15 +14,14 @@ str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 print_rev(str);
 return (0);
 }
-*/
 
-/**
- * print_array - print an array of integers
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
- */
+
+* print_array - print an array of integers
+* @a: an array of integers
+* @n: the number of elements to swap
+*
+* Return: nothing.
+
 void print_array(int *a, int n)
 {
 int i;
@@ -39,6 +38,22 @@ i++;
 printf("\n");
 }
 
+
+* main - check the code
+*
+* Return: Always 0.
+
+
+int main(void)
+{
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+print_array(a, sizeof(a) / sizeof(int));
+reverse_array(a, sizeof(a) / sizeof(int));
+print_array(a, sizeof(a) / sizeof(int));
+return (0);
+}
+*/
+
 /**
 * main - check the code
 *
@@ -47,9 +62,10 @@ printf("\n");
 
 int main(void)
 {
-int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-print_array(a, sizeof(a) / sizeof(int));
-reverse_array(a, sizeof(a) / sizeof(int));
-print_array(a, sizeof(a) / sizeof(int));
+char *s = "hello, world";
+char *f = "world";
+char *t;
+t = _strpbrk(s, f);
+printf("%s\n", t);
 return (0);
 }
---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _strpbrk - function that searches a string for any of a set of bytes.
+* @s: the pointer to the string.
+* @accept: an other string.
+* Return: the matches one of the byte or NULL if byte is not found.
+**/
+
+char *_strpbrk(char *s, char *accept)
+{
+int length = 0;
+int i,j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept [j])
+{
+length++;
+break;
+}
+}
+if (s[i] != accept[j])
+{
+return((char *)s);
+}
+}
+return (NULL);  
+}
