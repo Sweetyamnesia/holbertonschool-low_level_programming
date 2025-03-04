3cfbd1ecc436b35997e8d503996b1e0940030627
Sweetyamnesia <angela.r@live.fr>
2025-03-04 16:03:29 +0100

    modif file

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,17 +3,10 @@
 
 /*
 _puts_recursion : function that prints a string.
-@s: the pointer
+@s: the string
 */
 
 void _puts_recursion(char *s)
 {
-    int i = 0;
-    char s;
-    
-    for (i = 0; i < s; i++)
-    {
-        _putchar(s);
-    }
-    _putchar('\n');
+  return (*s);  
 }
