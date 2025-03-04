df1fb9c38b7b12af403afb0f6619de0c13b0e0c6
Sweetyamnesia <angela.rl@live.fr>
2025-03-04 14:12:57 +0000

    add directory recursion

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - function that prints the sum of two diagonals of a square.
+* @a: the pointer.
+* @size : the size of the square.
+**/
+
+void print_diagsums(int *a, int size)
+{
+int i = 0;
+
+}
