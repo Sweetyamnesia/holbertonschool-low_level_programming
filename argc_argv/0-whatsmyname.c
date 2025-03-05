569a1f68334b4bc023956999299552502cbdf6d3
Sweetyamnesia <angela.r@live.fr>
2025-03-05 14:32:28 +0100

    modif betty-doc

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
 * main - Function that prints the name of the file.
 * @argc: The number of things entered in a command line.
 * @argv: The array which hold the things that we enter in a command line.
-* Return : 0 on a success, 1 on a failure.
+* Return: Always 0 (Success)
 */
 
 int main(int argc, char *argv[])
