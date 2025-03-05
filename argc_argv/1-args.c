9795430ca89e58bf38597711e66ccb515eb3283e
Sweetyamnesia <angela.r@live.fr>
2025-03-05 14:08:32 +0100

    add files

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Function that prints the name of the file.
+* @argc: The number of things entered in a command line.
+* @argv: The array which hold the things that we enter in a command line.
+* Return : 0 on a success, 1 on a failure.
+*/
+
+int main(int argc, char *argv[])
+{
+printf("argc = %d", argc);
+printf('\n');
+return (0);
+}
