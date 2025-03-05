e219a451e6876ce2e1afd3a262dbedf3d900714b
Sweetyamnesia <angela.r@live.fr>
2025-03-05 14:47:04 +0100

    modif file

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 
 /**
-* main - Function that prints the name of the file.
-* @argc: The number of things entered in a command line.
-* @argv: The array which hold the things that we enter in a command line.
-* Return : 0 on a success, 1 on a failure.
+* main - Function that prints the number of arguments passed into it.
+* @argc: The number of arguments.
+* @argv: The vector of arguments.
+* * Return: Always 0 (Success)
 */
 
 int main(int argc, char *argv[])
 {
-printf("argc = %d", argc);
-printf('\n');
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("argv [%d] = %s\n", i, argv[i]);
+}
+printf("\n");
 return (0);
 }
