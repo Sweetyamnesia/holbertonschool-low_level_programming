1cbe7ce5eb579fe931c4ddd45a6f513ce91c3f95
Sweetyamnesia <angela.r@live.fr>
2025-03-07 03:14:32 +0100

    modif file

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,12 +10,19 @@
 
 int _sqrt_recursion(int n)
 {
+int i = 1;
 if (n < 0)
 {
 return (-1);
 }
-else
+else if (i * i > n)
 {
-return (-_sqrt_recursion(sqrt(n) + n));
+return (-1);
 }
+else if (i * i == n)
+{
+return (i);
+i++;
+}
+return (_sqrt_recursion(n));
 }
