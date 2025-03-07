c0372829da21d26a008219c6bf74bd03807a7856
Sweetyamnesia <angela.r@live.fr>
2025-03-07 14:55:57 +0100

    modif file with ChatGPT to understand the use of the other function sqrt_helper

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,26 +3,32 @@
 #include <math.h>
 
 /**
-* _sqrt_recursion - Function that returns the natural square root of a number.
-* @n: The number.
-* Return: -1 if n don't have the natural square root.
+* sqrt_helper - Recursively checks for the natural square root.
+* @n: The number to find the square root of.
+* @i: The current divisor being tested.
+*
+* Return: The natural square root of n, or -1 if n does not have one.
+*/
+
+int sqrt_helper(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (sqrt_helper(n, i + 1));
+}
+
+/**
+* _sqrt_recursion - Returns the natural square root of a number.
+* @n: The number to find the square root of.
+*
+* Return: The natural square root of n, or -1 if n does not have one.
 */
 
 int _sqrt_recursion(int n)
 {
-int i = 1;
 if (n < 0)
-{
 return (-1);
-}
-else if (i * i > n)
-{
-return (-1);
-}
-else if (i * i == n)
-{
-return (i);
-i++;
-}
-return (_sqrt_recursion(n));
+return (sqrt_helper(n, 1));
 }
