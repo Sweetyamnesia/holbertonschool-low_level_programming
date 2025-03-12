650426981fab76449eba13c821842b53c18529ff
Sweetyamnesia <angela.r@live.fr>
2025-03-12 16:38:30 +0100

    modif file : suppression du nouveau type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,15 +18,4 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-/**
-
-typedef struct dog
-{
-char *name;
-float age;
-char *owner;
-} dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-*/
 #endif
