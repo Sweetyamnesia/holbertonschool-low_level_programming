4129c5178a2094a4576ae63beaec7ebefc6019eb
Sweetyamnesia <angela.r@live.fr>
2025-03-12 11:44:03 +0100

    modif file dog.h : define new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,7 +7,6 @@
 * @name: the name of the dog.
 * @age: the age of the dog.
 * @owner: the owner of the dog.
-*/
 
 struct dog
 {
@@ -18,4 +17,12 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
 #endif
