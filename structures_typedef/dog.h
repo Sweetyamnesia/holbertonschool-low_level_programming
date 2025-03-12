2d122e22ad289288f488b80fe256afdb787c2975
Sweetyamnesia <angela.r@live.fr>
2025-03-12 16:41:29 +0100

    modif file : ajout new type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,7 +7,7 @@
 * @name: the name of the dog.
 * @age: the age of the dog.
 * @owner: the owner of the dog.
-*/
+
 struct dog
 {
 char *name;
@@ -17,5 +17,20 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+
+* struct dog - is to define the dog.
+* @name: the name of the dog.
+* @age: the age of the dog.
+* @owner: the owner of the dog.
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
