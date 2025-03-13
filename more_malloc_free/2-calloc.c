67cd978bfb3711ca909abf2eb82db44e53cbd94a
Sweetyamnesia <angela.r@live.fr>
2025-03-13 15:59:27 +0100

    modif file with help

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,11 +12,11 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *arr;
+char *arr;
 unsigned int i = 0;
 if (nmemb == 0 || size == 0)
 return (NULL);
-arr = (int *)malloc(nmemb * size);
+arr = malloc(nmemb * size);
 if (arr == NULL)
 return (NULL);
 for (i = 0; i < nmemb * size; i++)
