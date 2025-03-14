8edc90d7bcd9b2fbebb12e119f4479d2718df665
Sweetyamnesia <angela.r@live.fr>
2025-03-14 10:54:27 +0100

    modif file

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -17,13 +17,4 @@ return;
 d->name = name;
 d->age = age;
 d->owner = owner;
-if (d->name == NULL)
-printf("Name: (nil)\n");
-else
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-if (d->owner == NULL)
-printf("Owner: (nil)\n");
-else
-printf("Owner: %s\n", d->owner);
 }
