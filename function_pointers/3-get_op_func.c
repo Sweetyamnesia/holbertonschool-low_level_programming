d2bb19879bb1dd2e0de2322d4d3c963ce900c32c
Sweetyamnesia <angela.r@live.fr>
2025-03-17 15:38:49 +0100

    add all files

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <_stdlib.h>
+
+/**
+* get_op_func - select the correct function to perform the operation.
+* @s: the operator passed as argument.
+* Return: NULL if s doesn't match with the 5 operators.
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
