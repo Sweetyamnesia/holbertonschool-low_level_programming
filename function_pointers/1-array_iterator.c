3043770905114cd8bf22678b7654f3b0e5a1b48f
Sweetyamnesia <angela.r@live.fr>
2025-03-17 12:56:56 +0100

    add new file

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,8 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-putchar(size);
-action(*array);
+unsigned int i = 0;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+	action(array[i]);
+}
 }
 
 
