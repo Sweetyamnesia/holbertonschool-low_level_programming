8b0e073b34f57999b8ad31f10fe9928be9299541
Sweetyamnesia <angela.r@live.fr>
2025-03-21 11:15:20 +0100

    modif all the code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,41 +3,50 @@
 #include <stdio.h>
 
 /**
-* print_all - function that prints anything.
-* @format: list of types of arguments.
-*/
+ * print_all - Prints anything based on a given format.
+ * @format: The format string containing type specifiers.
+ */
 
 void print_all(const char * const format, ...)
 {
-va_list list;
-char c;
-int i = 0;
-float f;
-const char *s;
+	va_list list;
+	int i = 0;
+	char c;
+	int num;
+	float f;
+	const char *s;
+	char sep = 0;
 
-va_start(list, format);
-
-while (format[i])
-{
-	if (s == NULL)
+	va_start(list, format);
+	while (format && format[i] != '\0')
 	{
-	printf("(nil)");
+		if (sep)
+			printf(", ");
+		if (format[i] == 'c')
+		{
+			c = (char) va_arg(list, int);
+			printf("%c", c);
+		}
+		else if (format[i] == 'i')
+		{
+			num = va_arg(list, int);
+			printf("%d", num);
+		}
+		else if (format[i] == 'f')
+		{
+			f = (float) va_arg(list, double);
+			printf("%f", f);
+		}
+		else if (format[i] == 's')
+		{
+			s = va_arg(list, const char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+		}
+		sep = 1;
+		i++;
 	}
-
-	if (s != NULL)
-	{
-	c = va_arg(list, int);
-	printf("%c, ", c);
-	i = va_arg(list, int);
-	printf("%d, ", i);
-	f = va_arg(list, double);
-	printf("%f, ", f);
-	s = va_arg(list, const char*);
-	printf("%s", s);
-	}
-	i++;
-}
-
-printf("\n");
-va_end(list);
+	printf("\n");
+	va_end(list);
 }
