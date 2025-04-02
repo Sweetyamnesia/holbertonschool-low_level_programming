c660d538afd65d306a3cd7281601eb455162d4a9
Sweetyamnesia <angela.r@live.fr>
2025-04-02 09:39:03 +0200

    remove if condition anduse size_t

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -11,14 +11,14 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 	const dlistint_t *current = h;
 
 	while (current != NULL)
 	{
-		if (current->n >= 0)
 		count++;
 		current = current->next;
 	}
+
 	return (count);
 }
