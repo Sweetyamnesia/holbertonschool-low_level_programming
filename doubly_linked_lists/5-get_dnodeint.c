f3c732073ec2fa4bdb9754d838762ebeaac6d0be
Sweetyamnesia <angela.r@live.fr>
2025-04-02 14:26:28 +0200

    use while loop instead of for loop

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -17,9 +17,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 	return (NULL);
 
-	for (i = 0; i < index; i++)
+	while (head != NULL && i < index)
 	{
 		head = head->next;
+		i++;
 	}
 
 	if (i == index)
