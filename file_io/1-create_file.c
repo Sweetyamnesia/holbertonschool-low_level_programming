0e5daa859266a163299c958b68a0916c6c372255
Sweetyamnesia <angela.r@live.fr>
2025-04-04 15:52:56 +0200

    fix: update create_file function to handle empty files correctly

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,9 +20,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 
 	if (filename == NULL)
-	{
-		return (-1);
-	}
+	return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
@@ -30,12 +28,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+	close(fd);
+	return (1);
+	}
+
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
